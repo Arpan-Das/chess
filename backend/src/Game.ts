20abf93621a5a06a511c99858b87c558b2456589
Arpan <arpandas200015@outlook.com>
2025-12-31 21:12:40 +0530

    initial webshocket setup

---
+++ backend/src/Game.ts
@@ -0,0 +1,33 @@
+import { Chess } from "chess.js";
+import { WebSocket } from "ws";
+
+export class Game{
+    public player1: WebSocket;
+    public player2: WebSocket;
+    public board: Chess;
+    private startTime: Date;
+
+    constructor(player1:WebSocket, player2:WebSocket) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new Chess();
+        this.startTime = new Date();
+    }
+
+    makeMove(socket:WebSocket, move:string){
+        // validation here
+        // is it this user
+        // is move valid 
+
+        // update the bard
+        // push the move 
+
+
+        // check if game is over 
+
+
+        // send the updated board to both the player
+
+    }
+
+}
---
+++ backend/src/messages.ts
@@ -0,0 +1,2 @@
+export const INIT_GAME = "init__game";
+export const MOVE = "move";
